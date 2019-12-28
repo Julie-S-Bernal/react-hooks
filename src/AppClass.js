4cabe1e9a4e1131679180885e418f3eaa369fd11
“Julie-S-Bernal“ <Julies.bernal@gmail.coml>
2019-12-28 16:31:17 +0000

    toggling with use state

--- src/AppClass.js
+++ src/AppClass.js
@@ -3,22 +3,49 @@ import React, { Component } from 'react';
 class App extends Component {
 
   state = {
-    count: 0
+    count: 0,
+    isOn: false
   }
 
+  //  USE STATE
+  // incrementCount = () => {
+  //   this.setState({
+  //     count: this.state.count + 1
+  //   })
+  // }
+
+  // USE PREVIOUS STATE
   incrementCount = () => {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
+    this.setState(prevState => ({
+      count: prevState.count + 1
+    }));
+  };
 
+  toggleLight = () => {
+    this.setState(prevState => ({
+      isOn: !prevState.isOn
+    }));
+  }
 
 render() {
   console.log(this.state.count)
   return (
+    // New way to writte fragmemts
+    <>
+    <h2>Counter</h2>
     <button onClick={this.incrementCount}>
       I was clicked {this.state.count} times
     </button>
+    <h2>Toggle Light</h2>
+    <div
+      style={{
+        height:'50px',
+        width: '50px',
+        background: this.state.isOn ? 'green' : 'grey'
+      }}
+      onClick={this.toggleLight}
+    />
+    </>
    )
   }
 }
--- src/AppFunction.js
+++ src/AppFunction.js
@@ -1,17 +1,47 @@
 import React, { useState } from 'react'
 
 const App = () => {
+  // classes always require to be objects but functions do not
   const [count, setCount] = useState(0)
+  const [isOn, setIsOn ] = useState(false)
+
+// USE STATE
+//   const incrementCount = () => {
+//       setCount(count + 1 )
+//   }
+
+ // USE PREVIOUS STATE
 
   const incrementCount = () => {
-      setCount(count + 1 )
+      setCount(prevCount => prevCount + 1 )
+  }
+
+  const toggleLight = () => {
+    setIsOn(prevIsOn => !prevIsOn)
   }
 
 
 return (
-    <button onClick={incrementCount}>
-      I was clicked {count} times
-    </button>
+<>
+  <h2>Counter</h2>
+  <button onClick={incrementCount}>
+    I was clicked {count} times
+  </button>
+  <h2>Toggle Light</h2>
+  <img
+    src={
+        isOn 
+        ? 'https://icon.now.sh/highlight/fd0'
+        : 'https://icon.now.sh/highlight/aaa'
+    }
+    style={{
+      height:'50px',
+      width: '50px',
+    }}
+    alt='Flashlight'
+    onClick={toggleLight}
+  />
+</>
 )
 
 };
